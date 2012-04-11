7f23aa6a99cb555e6132bd242c3045d1f0b6d493
Ben Gelb <beg5670@gmail.com>
2012-04-11 01:27:52 -0400

    index page created

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import web
-
+import model 
 urls = (
         '/', 'index',
         )
@@ -7,9 +7,41 @@ urls = (
 render = web.template.render('templates/', base='layout')
 
 class index():
+    init_form = web.form.Form(
+                            web.form.Textbox('num_traits', web.form.notnull, web.form.regexp('^-?\d+$', 'Not a number.'), description='Number of Traits: '),
+                            web.form.Textbox('pop_size', web.form.notnull, web.form.regexp('^-?\d+$', 'Not a number.'), description='Population Size: '),
+                            web.form.Textbox('num_gen', web.form.notnull, web.form.regexp('^-?\d+$', 'Not a number.'), description='Number of Generations: '),
+                            web.form.Dropdown('markov_selection', ['beatles', 'beethovan', 'bach', 'handel', 'essenFolksong', 'haydn', 'josquin']),
+                            )
+
+    setting_form = web.form.Form(
+                                web.form.Dropdown('selection_type', [('roulette', 'Roulette Wheel'), ('tournament', 'Tournament')]),
+                                web.form.Textbox('elitism', description='Elitism:'),
+                                web.form.Textbox('crossover_points', description='Number of Crossover Points:'),
+                                web.form.Textbox('mutation_rate', description='Mutation Rate:'),
+                                )
+    
     def GET(self):
-        title = 'Mmm Espresso'
-        return render.index(title)
+        # clear table
+        model.clear_tables()
+        
+        title = 'Melody Composer'
+        init_f = self.init_form()
+        sf = self.setting_form()
+        
+        return render.index(title, init_f, sf)
+
+    def POST(self):
+        init_f = init_form()
+        
+        if not init_form.validates():
+            return render.index(init_f) 
+        else:
+            fp = web.input()       
+            print fp.selection_type    
+            model.insert_param(fp.num_traits, fp.pop_size, fp.num_gen)
+            #raise web.seeother('/spawn_pop')
+            spawn_pop().init(fp.markov_selection)
 
 if __name__ == "__main__":
    app = web.application(urls, globals())
