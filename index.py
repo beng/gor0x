79bf541594c9cd3f583b3f9f2df88fa674ca326f
Ben Gelb <beg5670@gmail.com>
2012-04-18 15:22:02 -0400

    fixed comments and added genetic operations

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ import random
 import song_name
 from markov import markov
 from music21 import *
+import selection
+
 urls = (
         '/', 'index',
         '/fitness/(.*)', 'fitness',
@@ -108,12 +110,21 @@ class fitness():
                              )
     
     def to_list(self, q):
+        ''' 
+        currently use this and not map(str,q) because it is easier to read and
+        cleaner looking to parse the traits while using a forloop than to use
+        a single line functional approach
+        '''
         ret = []
         for i in q:
             ret += [(i.pitch, i.duration)]
         return ret
     
     def create_pheno(self, indi):
+        '''
+        converts the individuals pitch, accidental, octave, and rhythm to a music stream
+        using the music21 library. the music stream is then used to create a midi file
+        '''
         gene = self.to_list(model.get_indi_traits(indi))
         partupper = stream.Part()
         m = stream.Measure()
@@ -127,6 +138,9 @@ class fitness():
         return partupper
     
     def convert_midi(self, mfile, indi_id):
+        '''
+        mfile is a musicstream which is exported to as midi format
+        '''
         mf = mfile.midiFile
         name = str(indi_id) + 'song.mid'
         mf.open('static/' + name, 'wb')
@@ -178,6 +192,121 @@ class helper():
         # just in case exit condition
         else:
             raise web.seeother('/terminate')       
+
+#===============================================================================
+# SELECT
+"""
+genetic operator - selection
+only tournament selection implemented right now
+
+check each method for algorithm details
+"""
+#===============================================================================
+class select():
+    def select(self, indi):
+        '''
+        select
+            indi = current indi id
+            
+        1. determine the type of selection the user requested
+        2. call the requested method
+        3. use the winner to call crossover
+        '''
+        num_rounds = 2
+        k = 2
+        winner = []
+        # need to make sure num_rounds is an even number
+        for i in range(0, num_rounds):
+            winner += [self.tournament(k, indi)]
+        
+        # call crossover on winner        
+        crossover().crossover(random.choice(winner), random.choice(winner), 1)
+        
+        # mutate
+        mutate().mutate(random.choice(winner))
+        
+        raise web.seeother('/fitness/' + str(int(indi) + 1))
+    
+    def tournament(self, k, indi):
+        '''
+        tournament selection
+            k = subset size
+            indi = current indi id -- used for future generation and id number
+        
+        1. a random subset of size, k, from the given generation is extracted 
+        2. sort the pool by fitness value
+        3. return the winner, the individual with the highest fitness value
+        '''
+        # from each individual in the current generation: get the indi_id and fitness value
+        tmp = model.get_all_indi_id_by_gen(model.get_cur_gen(indi - 1))
+        all = []
+        for i in tmp:
+            # put each indi and fitness in tuple
+            all += [(i.indi_id, i.fitness)]
+        
+        # randomly select k individuals to create pool
+        pool = []
+        for i in range(0, k):
+            if random.choice(all) not in pool:
+                pool += [random.choice(all)]
+        print 'pool == ', pool
+        
+        # select indi with highest fitness as winner
+        winner = sorted(pool, key=lambda x:-x[1])[0][0]
+        print 'the winner id is ', winner
+        return winner
+   
+    def elitism(self):
+        '''
+        based on the elitism percent, the top individual has a % chance
+        of skipping mutation/selection/crossover and automatically copied into 
+        the next generation
+        '''
+        # todo
+        return 0
+    
+    
+#===============================================================================
+# CROSSOVER
+"""
+genetic operator - crossover
+
+only single point crossover implemented right now
+    -split is randomly selected
+check each method for algorithm details
+"""
+#===============================================================================
+class crossover():
+    def crossover(self, p1, p2, point):    
+        # get traits for each parent
+        p1t = self.to_list(model.get_indi_traits(p1))
+        p2t = self.to_list(model.get_indi_traits(p2))
+        split = random.randint(1, model.get_num_traits())
+        
+        # create each child
+        c1 = p1t[:split] + p2t[split:]
+        c2 = p2t[:split] + p1t[split:]
+        
+        # store children in database        
+        tmp_id = int(model.get_max_indi_id()) + 1
+        tmp_gen = int(model.get_max_gen()) + 1
+        
+        for i in c1:
+            model.insert_trait(tmp_id, tmp_gen, i[0], i[1])
+             
+        for i in c2:
+            model.insert_trait(tmp_id + 1, tmp_gen, i[0], i[1])
+    
+    def to_list(self, q):
+        ''' 
+        currently use this and not map(str,q) because it is easier to read and
+        cleaner looking to parse the traits while using a forloop than to use
+        a single line functional approach
+        '''
+        ret = []
+        for i in q:
+            ret += [(str(i.pitch), str(i.duration))]
+        return ret
         
 if __name__ == "__main__":
    app = web.application(urls, globals())
