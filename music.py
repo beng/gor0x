15193f4a334e67a13ca2021a7e87968d8193083c
Ben Gelb <beg5670@gmail.com>
2012-07-07 23:50:56 -0400

    boilerplate crap

--- music.py
+++ music.py
@@ -1,3 +1,5 @@
+import sys
+
 """
 initial population
     -supply either artist name or 2 songs 
@@ -11,7 +13,20 @@ fitness
     -winners of round =  30%similar, 30%X middle, and 60% different children depending on desired number of children.       this will be useful to ensure that the population doesnt converge 
     -use case:
         the 2 songs supplied on the cmd line are "billy joel - we didnt start the fire" and "jay-z - hard knock life". 
-        take the euclidean distance between those 2 songs and use that to compare against the SUM of the euclidean distance between the child and billy joel, and the euclidean distance between the child and jay-z.  
+        take the euclidean distance between those 2 songs and use that to compare against the SUM of the euclidean distance between the child and billy joel, and the euclidean distance between the child and jay-z.       
 """
 
 
+def run():
+	pass
+
+def main(args):
+    try:
+    	run()
+        # implement opts, args = getopt.getopt to allow flags and args
+        # e.g. python music.py -a <artist> -s1 <song_1> -s2 <song_2>
+    except ValueError:
+		pass
+    
+if __name__ == '__main__':	
+	main(sys.argv[1:])
