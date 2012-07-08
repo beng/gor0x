a496d8c8e39c4506f6b178c4deca9c7c49cb77be
Ben Gelb <beg5670@gmail.com>
2012-07-08 00:27:36 -0400

    clean up in aisle 2

--- individual.py
+++
@@ -1,9 +0,0 @@
-import random
-import markov
-
-def individual(m, size):
-    duration = ['whole', 'half', 'quarter', 'eighth', '16th']
-    return m.generate_music(size)
-
-def population(m, size):
-	return [individual(m,size) for i in range(size)]
