246632a6cc5166253322cbe737dcda5ca0f7da80
Ben Gelb <beg5670@gmail.com>
2012-08-21 17:15:06 -0400

    removed old files

--- ga/fitness.py
+++
@@ -1,7 +0,0 @@
-from music21 import * # horrible, i know, but very lazy right now!
-
-class ManualFitness:
-	def euclid_distance(song1, song2):
-		# .midi maps the PitchAccidentalOctave (C#3) to the midi equivalent
-		# clean the fuck up ASAP!
-		return sum([(math.sqrt((pitch.Pitch(song1[i]).midi - pitch.Pitch(song2[i]).midi)) ** 2) for i in range(len(song1))])
