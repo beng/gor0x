68f855eb28428e80466c9aca7c7a899c06591cb5
Ben Gelb <beg5670@gmail.com>
2012-08-25 11:20:11 -0400

    utility functions

--- src/helper/utility.py
+++ src/helper/utility.py
@@ -2,6 +2,3 @@ def msg(*args):
     """Print out message with variables
     Usage: msg('text',variable,...)"""
     return "".join(str(x) for x in args)
-
-
-msg = Str("I found ", filecount, " files in ", foldercount, " directories" )
