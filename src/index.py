120592b335c1ec8a09283c0215593e7ec6d0d527
Ben Gelb <beg5670@gmail.com>
2012-08-27 22:12:36 -0400

    formatting cleanup

--- src/index.py
+++ src/index.py
@@ -65,9 +65,10 @@ class ExtractMidi:
 
         if not influencer:
             influencer=consts.name
-            
+
         try:
-            parsed_corpus = utility.extract_traits(utility.extract_corpus(influencer), traits=[note.Note,chord.Chord, 'duration'])  # duration can be any name becuase we are just checking for type
+            parsed_corpus = utility.extract_traits(utility.extract_corpus(influencer), 
+                traits=[note.Note,chord.Chord, 'duration'])  # duration can be any name becuase we are just checking for type
             web.header('Content-Type', 'application/json')
             return json.dumps({influencer : parsed_corpus})
         except Exception, e:
