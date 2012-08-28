5d733e087968da0a79251243d93cdff0c17e0272
Ben Gelb <beg5670@gmail.com>
2012-08-27 20:00:30 -0400

    formatting cleanup

--- src/ga.py
+++ src/ga.py
@@ -2,9 +2,6 @@ from helper.markov import MarkovChain
 import helper.consts as consts
 
 
-'''
-Generate an individual (genome) from the given corpus
-'''
 def genome(corpus, *args):
     """Generate a genome
     *args MAY contain size and nodes"""
@@ -13,23 +10,13 @@ def genome(corpus, *args):
 
     return Markov().markov_pitch(corpus)
 
-
-'''
-Take a dictionary containing the 
-desired population size, number of traits,
-and influencers and return an initial
-population
-'''
-
-'''
-Generate a Markov chain
-'''
 class Markov:
+    """Generate a Markov chain"""
+
     def markov_pitch(self, corpus, size=5000, nodes=5):
         """Nodes = number of previous nodes to remember"""    
         m = self.walk_corpus(corpus, nodes)
         return [''.join([next(m) for k in xrange(size)])]
-
 
     def walk_corpus(self, corpus, nodes):
         chain = MarkovChain(nodes)
