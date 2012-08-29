abde77929e3aff54f68afb4f03dde3aac41eac95
Ben Gelb <beg5670@gmail.com>
2012-08-28 22:48:12 -0400

    wasn"t using the model anyway

--- src/model.py
+++ src/model.py
@@ -1,19 +1,23 @@
 import web
 
-dbn = 'sqlite'
-db = './db/community.db'
-db = web.database(dbn=dbn, db=db)
+"""
+Fuck it. Shit ain't even used anymore.
+"""
 
-def clear_tables():
-    db.query("delete from params")
-    db.query("delete from song")
+# dbn = 'sqlite'
+# db = './db/community.db'
+# db = web.database(dbn=dbn, db=db)
 
-def insert(table, kargs):
-    return db.insert(table, **kargs)
+# def clear_tables():
+#     db.query("delete from params")
+#     db.query("delete from song")
 
-def get_max_gen():
-    q = db.query("SELECT max(generation) as gen FROM song")[0].gen
-    return q if q else 0
+# def insert(table, kargs):
+#     return db.insert(table, **kargs)
 
-def get_traits(indi_id):
-    return db.select('song', what='trait', vars=indi_id, where='indi_id=$indi_id')
+# def get_max_gen():
+#     q = db.query("SELECT max(generation) as gen FROM song")[0].gen
+#     return q if q else 0
+
+# def get_traits(indi_id):
+#     return db.select('song', what='trait', vars=indi_id, where='indi_id=$indi_id')
