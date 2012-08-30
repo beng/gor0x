e1fbd16c9c8eee8cdf3490940c44ae894345e621
Ben Gelb <beg5670@gmail.com>
2012-08-30 19:41:20 -0400

    fixed some stuff with extract_traits. still a cluster fuck though

--- src/helper/utility.py
+++ src/helper/utility.py
@@ -2,8 +2,10 @@
 Different utility functions
 """
 
+import json
 import music21
 import itertools
+
 
 ########################################################
 # MIDI
@@ -12,28 +14,33 @@ def extract_corpus(song):
     """Convert midi file into json object)"""
     return music21.converter.parse(song)
 
-def extract_traits(corpus, traits=[music21.note.Note]):
+def extract_traits(corpus, influencer, traits=[]):
     """Duration will always be included for the time being!
     Everything is casted to string because JSON throws an error
-    otherwise"""
-    # @TODO add what to do with wrong traits!
-    # @TODO decide if i want to use a generator instead
-
-    trait_list = []
-    for stream in corpus:
-        for element in stream.elements:
-            if type(element) in traits:
-                if type(element) == music21.note.Rest:
-                    trait_list.append({str(type(element)) : "rest", "duration" : element.duration.type})
-                else:
-                    trait_list.append({str(type(element)) : str(element.pitches), "duration" : element.duration.type})
-
-    return trait_list
+    otherwise"""    
+    if not traits:
+        return {}   
+    else:
+        trait_list = {"influencer" : influencer, "info" : []}
+        for stream in corpus:
+            for element in stream.elements:
+                if type(element) in traits:
+                    if type(element) == music21.note.Rest:
+                        trait_list['info'].append({"Rest" : "rest", "duration" : element.duration.type})
+                    else:
+                        trait_list['info'].append({str(type(element)) : str(element.pitches), "duration" : element.duration.type})
+        return trait_list
 
 def save_traits(fn, loc, traits):
-    """Save list of traits to json file
-    Complains if i add '.close()'"""
-    open(loc + fn + '.json', 'wb').write(str(traits))   # hack for time being -- needs string/buffer not list to write
+    with open(loc + fn + '.json', 'wb') as fp:
+        json.dump(traits, fp)
+
+def load_traits(fn, loc):
+    with open(loc + fn + '.json', 'rb') as fp:
+        traits = json.load(fp)
+        #print traits
+        print type(traits)
+        return traits
 
 ########################################################
 # Strings
@@ -49,14 +56,23 @@ def to_string(items):
 ########################################################
 # Dictionary
 ########################################################
-def find_item(info, *args):
-    """Usage: utility.find_item(info, 'chord',...):
-    info = list of dictionaries containing song traits
-    args = the requested traits"""
-    for i in info:
-        #print 'in first loop, iteration: '
-        for k,v in i.iteritems():
-            #print 'checking k,v in iteration: '
-            if k in args:
-                yield {k:v},
+# def find_item(info, *args):
+#     """Usage: utility.find_item(info, 'chord',...):
+#     info = list of dictionaries containing song traits
+#     args = the requested traits"""
+#     for i in info:
+#         #print 'in first loop, iteration: '
+#         for k,v in i.iteritems():
+#             #print 'checking k,v in iteration: '
+#             if k in args:
+#                 yield {k:v},
+
+# def find_item(info, *args):
+#     for k,v in info.items():
+#         if k in args[0]:
+#             for i in v:
+#                 print i.keys()
+#                 if i in args[1:]:
+#                     print i
+#                 #print i[args[1:]]
 
