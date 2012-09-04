de94f6b6bb91eadab9ca2241d3677717a66c7ac0
Ben Gelb <beg5670@gmail.com>
2012-09-04 16:10:43 -0400

    Added music collection format

--- src/model.py
+++ src/model.py
@@ -2,14 +2,35 @@ import web
 from pymongo import Connection
 
 """
-Fuck it. Shit ain't even used anymore.
+BSON Structure:
+    { artist: song,
+      id: {
+            note: note.pitch,
+            duration: duration.type,
+      }
+    }
+
+E.g. { "Antonio Vivaldi": "Winter Allegro",
+        0: {
+             "note": "C#4",
+             "duration": "quarter",
+        }
+     }
 """
 
 host = 'localhost'
-port = '9999'
+port = 9999
 db_name = 'community'
 
 connection = Connection(host, port)
 db = connection[db_name]
 music_coll = db['music_collection']
 
+def add_music(name, song):
+    music_coll.insert({name: song})
+
+def find_all_music():
+    return music_coll.find_one()
+
+add_music('Vivaldi', 'Winter Allegro')
+print find_all_music()
