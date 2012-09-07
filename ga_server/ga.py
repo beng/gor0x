9d4a8dd8229914f86a48acd3544226d364224e67
Ben G <beg5670@gmail.com>
2012-09-07 12:38:25 -0400

    fixed a formatting issue across the client and server based on spaces

--- ga_server/ga.py
+++ ga_server/ga.py
@@ -9,36 +9,36 @@ from pyevolve import Initializators
 from pyevolve import GAllele
 
 def eval_func(chromosome):
-	return random.randint(0,100)
+    return random.randint(0,100)
 
 def init_ga(num_traits):
-	"""Shit is acting funky right now. I'm doing something stupid.
-	The problem might be with using the GAllele structure. I'll
-	experiment with this later once I get the real fitness function
-	in place."""
+    """Shit is acting funky right now. I'm doing something stupid.
+    The problem might be with using the GAllele structure. I'll
+    experiment with this later once I get the real fitness function
+    in place."""
 
-	setOfAlleles = GAllele.GAlleles()
-	num_traits = int(num_traits)
+    setOfAlleles = GAllele.GAlleles()
+    num_traits = int(num_traits)
 
-	for i in model.pop_find_all():
-		a = GAllele.GAlleleList(i['note'])
-		setOfAlleles.add(a)
+    for i in model.pop_find_all():
+        a = GAllele.GAlleleList(i['note'])
+        setOfAlleles.add(a)
 
-	genome = G1DList.G1DList(num_traits)
+    genome = G1DList.G1DList(num_traits)
 
-	genome.setParams(allele=setOfAlleles)
-	genome.evaluator.set(eval_func)
-	genome.mutator.set(Mutators.G1DListMutatorAllele)
-	genome.initializator.set(Initializators.G1DListInitializatorAllele)
+    genome.setParams(allele=setOfAlleles)
+    genome.evaluator.set(eval_func)
+    genome.mutator.set(Mutators.G1DListMutatorAllele)
+    genome.initializator.set(Initializators.G1DListInitializatorAllele)
 
-	# Genetic Algorithm Instance
-	ga = GSimpleGA.GSimpleGA(genome)
-	ga.selector.set(Selectors.GRouletteWheel)
-	ga.setGenerations(20)
+    # Genetic Algorithm Instance
+    ga = GSimpleGA.GSimpleGA(genome)
+    ga.selector.set(Selectors.GRouletteWheel)
+    ga.setGenerations(20)
 
-	# Do the evolution, with stats dump
-	# frequency of 10 generations
-	ga.evolve(freq_stats=1)
+    # Do the evolution, with stats dump
+    # frequency of 10 generations
+    ga.evolve(freq_stats=1)
 
-	# Best individual
-	print ga.bestIndividual()
+    # Best individual
+    print ga.bestIndividual()
--- ga_server/index.py
+++ ga_server/index.py
@@ -13,48 +13,48 @@ app = web.application(urls, globals())
 title = 'GA Server'
 
 class Index:
-	def GET(self):
-		# clear population collection
-		model.pop_clear_conn()
+    def GET(self):
+        # clear population collection
+        model.pop_clear_conn()
 
-		# parameters for calling the server
-		route = 'spawn_pop'
-		artist = 'Vivaldi'
-		song = 'winter_allegro'
-		num_indi = '1'
-		num_traits = '4'
-		size = '5000'
-		nodes = '5'
-		params = [route, artist, song, num_indi, num_traits, size, nodes]
-		params = '/'.join(params)
+        # parameters for calling the server
+        route = 'spawn_pop'
+        artist = 'Vivaldi'
+        song = 'winter_allegro'
+        num_indi = '1'
+        num_traits = '4'
+        size = '5000'
+        nodes = '5'
+        params = [route, artist, song, num_indi, num_traits, size, nodes]
+        params = '/'.join(params)
 
-		br = web.Browser()
-		br.open(params)
-		traits = json.loads(br.get_text())
+        br = web.Browser()
+        br.open(params)
+        traits = json.loads(br.get_text())
 
-		# add content to population collection
-		for trait in traits:
-			model.pop_save_individual(trait)
+        # add content to population collection
+        for trait in traits:
+            model.pop_save_individual(trait)
 
-		# call pyevolve class to initialize everything
-		return ga.init_ga(num_traits)
+        # call pyevolve class to initialize everything
+        return ga.init_ga(num_traits)
 
 class Fitness:
-	def GET(self):
-		pass
+    def GET(self):
+        pass
 
 # class Population:
-# 	"""Generate a population"""
+#   """Generate a population"""
 
-# 	def GET(self):
-# 		params = ['markov', '5000', '5', 'Vivaldi', 'winter_allegro']
-# 		params = '/'.join(params)
+#   def GET(self):
+#       params = ['markov', '5000', '5', 'Vivaldi', 'winter_allegro']
+#       params = '/'.join(params)
 
-# 		br = web.Browser()
-# 		br.open(params)
-# 		traits = eval(br.get_text())	# string to list
+#       br = web.Browser()
+#       br.open(params)
+#       traits = eval(br.get_text())    # string to list
 
-	
+    
 if __name__ == "__main__":
    app.internalerror = web.debugerror
    app.run() 
